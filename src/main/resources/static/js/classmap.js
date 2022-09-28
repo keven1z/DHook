3a2bc9eff8f94b26fbd6337ba975ea33413f8813
keven1z <ziizhuwy@gmail.com>
2022-09-28 17:35:20 +0800

    commit for 2.5

--- src/main/resources/static/js/classmap.js
+++ src/main/resources/static/js/classmap.js
@@ -1,17 +1,17 @@
 var HtmlUtil = {
     /*1.用浏览器内部转换器实现html转码*/
-    htmlEncode:function (html){
+    htmlEncode: function (html) {
         //1.首先动态创建一个容器标签元素，如DIV
-        var temp = document.createElement ("div");
+        var temp = document.createElement("div");
         //2.然后将要转换的字符串设置为这个元素的innerText(ie支持)或者textContent(火狐，google支持)
-        (temp.textContent !== undefined ) ? (temp.textContent = html) : (temp.innerText = html);
+        (temp.textContent !== undefined) ? (temp.textContent = html) : (temp.innerText = html);
         //3.最后返回这个元素的innerHTML，即得到经过HTML编码转换的字符串了
         var output = temp.innerHTML;
         temp = null;
         return output;
     },
     /*2.用浏览器内部转换器实现html解码*/
-    htmlDecode:function (text){
+    htmlDecode: function (text) {
         //1.首先动态创建一个容器标签元素，如DIV
         var temp = document.createElement("div");
         //2.然后将要转换的字符串设置为这个元素的innerHTML(ie，火狐，google都支持)
@@ -22,6 +22,7 @@ var HtmlUtil = {
         return output;
     }
 };
+
 function load_classMap() {
     var url = "/classmap/all?agentId=" + $("#small").text();
     $.ajax({
@@ -54,6 +55,7 @@ function load_classMap() {
 }
 
 function get_class_info() {
+    $(".spinner-border").show();
     let url = "/classmap/class/target/add?className=" + $('#class').text().replace("Class ", "") + "&packageName=" + $('#packageName').text()
     $.ajax({
         //async:false,非异步，modal窗口失效；
@@ -87,7 +89,8 @@ function receive_info() {
             return;
         }
         let super_class = data.superClass
-        if (super_class == null){
+        $(".spinner-border").hide()
+        if (super_class == null) {
             alert("未获取到类信息")
         }
         let interfaces = data.interfaces
@@ -111,13 +114,18 @@ function receive_info() {
         if (methods === "" || methods == null) return;
         let m = methods.split("#")
         for (let i = 0; i < m.length; i++) {
-                let _ = m[i].split(" ");
-            let desc = "<tr><td>" + HtmlUtil.htmlEncode(_[0])+ "</td><td>" + HtmlUtil.htmlEncode(_[1]) + "</td></tr>"
+            let _ = m[i].split(" ");
+            let desc = "<tr><td>" + HtmlUtil.htmlEncode(_[0]) + "</td><td>" + HtmlUtil.htmlEncode(_[1]) + "</td></tr>"
             methodBody.append(desc);
         }
+
     })
 }
-function get_code(){
+
+function get_code() {
+    $(".spinner-border").show();
     let url = "/classmap/class/code/get?className=" + $('#class').text().replace("Class ", "") + "&packageName=" + $('#packageName').text()
-    window.open(url,'_blank');
+    window.open(url, '_blank');
+    $(".spinner-border").hide();
+
 }
